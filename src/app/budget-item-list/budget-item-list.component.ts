b47612c287154a678d5f5b6e8145cf017767a721
schvbertt <diamenttx@gmail.com>
2020-05-02 13:23:40 +0200

    Prototype

---
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item-model';
+import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+@Component({
+  selector: 'app-budget-item-list',
+  templateUrl: './budget-item-list.component.html',
+  styleUrls: ['./budget-item-list.component.scss']
+})
+
+export class BudgetItemListComponent implements OnInit {
+
+  @Input() budgetItems: BudgetItem[];
+  @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor( public dialog: MatDialog ) { }
+
+  ngOnInit(): void {
+  }
+
+  onDeleteButtonClicked(item: BudgetItem) {
+    this.delete.emit(item);
+  }
+
+  onCardClicked(item: BudgetItem) {
+    let dialogRef = this.dialog.open(EditItemModalComponent, {
+      width: '580px',
+      data: item
+    })
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        // this.budgetItems[this.budgetItems.indexOf(item)] = result;
+        this.update.emit({
+          old: item,
+          new: result
+        });
+      }
+    })
+
+
+  }
+}
+
+export interface UpdateEvent {
+  old: BudgetItem;
+  new: BudgetItem;
+}
